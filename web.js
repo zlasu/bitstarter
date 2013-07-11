d172e9b68cb4439221058bb32c2c48ff713ed4b8
zlasu <praca.rails@gmail.com>
2013-07-11 08:44:41 +0200

    part 3 web

--- web.js
+++ web.js
@@ -2,12 +2,9 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-
 app.get('/', function(request, response) {
-	
 	fs = require('fs');
 	var buf = fs.readFileSync("index.html");
-	
   	response.send(buf.toString());
 });
 
